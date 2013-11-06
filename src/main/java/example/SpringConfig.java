b5373747377c05ecde0b5a968f5ab9b01c6b86d6
Steve Hanson <s.hanson5@gmail.com>
2013-11-06 14:05:09 -0600

    Fix package for component scan

--- src/main/java/example/SpringConfig.java
+++ src/main/java/example/SpringConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("test")
+@ComponentScan("example")
 public class SpringConfig {
 
 	
